e23d7ceebc5eb89bf480878dc1980fa5495a91ec
WillTheBigCodeMan <121814654+WillTheBigCodeMan@users.noreply.github.com>
2024-01-02 12:38:40 +0000

    Add files via upload

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-const envConfig = require("dotenv").config();
+//const envConfig = require("dotenv").config();
 const express = require("express");
 const Ably = require("ably");
 const app = express();
@@ -73,44 +73,102 @@ async function matchmaking() {
         await matchmakingChannel.publish(queue[i][0], ("gameChannel" + currentGames.length.toString()));
       }
       gameChannels.push(realtime.channels.get("gameChannel" + currentGames.length.toString()));
-      currentGames.push([queue[0][1], queue[1][1], 0]);
+      currentGames.push(new Game(queue[0][1], queue[1][1], 0));
       queue.splice(0, 2);
       gameLoop();
     }
   });
 }
 
+class Game{
+  constructor(p1, p2, cP){
+    this.player1 = p1;
+    this.player2 = p2;
+    this.connectedPlayers = cP;
+    this.board = [];
+    this.turn = 0;
+    this.revealed = [];
+  }
+  
+  generateBlank(){
+    for(let i = 0; i < 15; i++){
+      this.board.push([]);
+      this.revealed.push([]);
+      for(let j = 0; j < 15; j++){
+        this.board[i].push(-2);
+        this.revealed[i].push(false);
+      }
+    }
+  }
+
+  genBoard(fX, fY) {
+    for (let i = -1; i <= 1; i++) {
+      for (let j = -1; j <= 1; j++) {
+        if (fX + i >= 0 && fY + j >= 0 && fX + i < width && fY + j < height) {
+          this.board[fX + i][fY + j] = 0;
+        }
+      }
+    }
+    for (let i = 0; i < mines; i++) {
+      let x = Math.floor(Math.random() * width);
+      let y = Math.floor(Math.random() * height);
+      while (this.board[x][y] !== -2) {
+        x = Math.floor(Math.random() * width);
+        y = Math.floor(Math.random() * height);
+      }
+      this.board[x][y] = -1;
+    }
+    for (let i = 0; i < width; i++) {
+      for (let j = 0; j < height; j++) {
+        if (this.board[i][j] < -1) {
+          this.board[i][j] = 0;
+        }
+        if (this.board[i][j] >= 0) {
+          for (let k = -1; k <= 1; k++) {
+            for (let l = -1; l <= 1; l++) {
+              if ((i + k) >= 0 && (j + l) >= 0 && (i + k) < width && (j + l) < height) {
+                if (this.board[i + k][j + l] == -1) {
+                  this.board[i][j]++;
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
+  incrementTurn(){
+    this.turn++;
+  }
+
+  reveal(x,y){
+    this.revealed[x][y] = true;
+  }
+}
+
 async function gameLoop() {
   for (let i = 0; i < gameChannels.length; i++) {
     gameChannels[i].presence.subscribe("enter", async (player) => {
-      currentGames[i][2]++;
-      if (currentGames[i][2] == 2) {
-        currentGames[i].push([]);
-        currentGames[i].push(0);
-        currentGames[i].push([]);
-        for (let ii = 0; ii < 15; ii++) {
-          currentGames[i][3].push([]);
-          currentGames[i][5].push([]);
-          for (let j = 0; j < 15; j++) {
-            currentGames[i][3][ii].push(-2);
-            currentGames[i][5][ii].push(false);
-          }
-        }
-        await gameChannels[i].publish("commands", { p1: currentGames[i][0], p2: currentGames[i][1], identifier: "playerRoles" })
+      currentGames[i].connectedPlayers++;
+      if (currentGames[i].connectedPlayers == 2) {
+        console.log("aa");
+        currentGames[i].generateBlank();
+        await gameChannels[i].publish("commands", { p1: currentGames[i].player1, p2: currentGames[i].player2, identifier: "playerRoles" })
       }
     });
     gameChannels[i].subscribe("input", async (message) => {
       console.log(message.data);
-      if (currentGames[i][4] == 0 && message.data.player == currentGames[i][0]) {
-        currentGames[i][3] = genBoard(message.data.position.x, message.data.position.y, currentGames[i][3]);
+      if (currentGames[i].turn == 0 && message.data.player == currentGames[i].player1) {
+        currentGames[i].genBoard(message.data.position.x, message.data.position.y);
       }
-      if (currentGames[i][4] % 2 == 0 && message.data.player == currentGames[i][0]&&message.data.type == "left") {
-        if(currentGames[i][3][message.data.position.x][message.data.position.y] == -1){
+      if (currentGames[i].turn % 2 == 0 && message.data.player == currentGames[i].player1 && message.data.type == "left") {
+        if(currentGames[i].board[message.data.position.x][message.data.position.y] == -1){
           await gameChannels[i].publish("commands", {winner:"bomber", identifier:"winLoss"});
         }
-        currentGames[i][4] += 1;
-        let fullRevealed = waterfall(message.data.position.x, message.data.position.y, currentGames[i][3], currentGames[i][5]);
-        fullRevealed.push({x:message.data.position.x, y:message.data.position.y, val:currentGames[i][3][message.data.position.x][message.data.position.y]});
+        currentGames[i].incrementTurn();
+        let fullRevealed = waterfall(message.data.position.x, message.data.position.y, currentGames[i]);
+        fullRevealed.push({x:message.data.position.x, y:message.data.position.y, val:currentGames[i].board[message.data.position.x][message.data.position.y]});
         let newRevealed = [];
         for (let i = 0; i < fullRevealed.length; i++) {
           let valid = true;
@@ -131,14 +189,13 @@ async function gameLoop() {
         newRevealed.push(totVal);
         await gameChannels[i].publish("solver", newRevealed);
       }
-      if (currentGames[i][4] % 2 == 1 && message.data.player == currentGames[i][1]&&message.data.type == "left") {
-        currentGames[i][4] += 1;
-        console.log(message.data);
-        if (currentGames[i][3][message.data.position.x][message.data.position.y] == -1) {  
+      if (currentGames[i].turn % 2 == 1 && message.data.player == currentGames[i].player2&&message.data.type == "left") {
+        currentGames[i].incrementTurn();
+        if (currentGames[i].board[message.data.position.x][message.data.position.y] == -1) {  
           await gameChannels[i].publish("commands", {winner:"bomber", identifier:"winLoss"});
           console.log("BOMB WIN");
         } else {
-          let revealed = [{ x: message.data.position.x, y: message.data.position.y, value: currentGames[i][3][message.data.position.x][message.data.position.y]}]
+          let revealed = [{ x: message.data.position.x, y: message.data.position.y, value: currentGames[i].board[message.data.position.x][message.data.position.y]}]
           await gameChannels[i].publish("bomber", revealed);
         }
       }
@@ -146,7 +203,7 @@ async function gameLoop() {
     gameChannels[i].subscribe("solverSelection", async (message) => {
       let revealed = [];
       for (let j = 0; j < message.data.length; j++) {
-        revealed.push({ x: message.data[j].pos.x, y: message.data[j].pos.y, value: currentGames[i][3][message.data[j].pos.x][message.data[j].pos.y] });
+        revealed.push({ x: message.data[j].pos.x, y: message.data[j].pos.y, value: currentGames[i].board[message.data[j].pos.x][message.data[j].pos.y] });
       }
       await gameChannels[i].publish("bomber", revealed)
     });
@@ -155,60 +212,22 @@ async function gameLoop() {
 
 matchmaking();
 
-function genBoard(fX, fY, board) {
-  for (let i = -1; i <= 1; i++) {
-    for (let j = -1; j <= 1; j++) {
-      if (fX + i >= 0 && fY + j >= 0 && fX + i < width && fY + j < height) {
-        board[fX + i][fY + j] = 0;
-      }
-    }
-  }
-  for (let i = 0; i < mines; i++) {
-    let x = Math.floor(Math.random() * width);
-    let y = Math.floor(Math.random() * height);
-    while (board[x][y] !== -2) {
-      x = Math.floor(Math.random() * width);
-      y = Math.floor(Math.random() * height);
-    }
-    board[x][y] = -1;
-  }
-  for (let i = 0; i < width; i++) {
-    for (let j = 0; j < height; j++) {
-      if (board[i][j] < -1) {
-        board[i][j] = 0;
-      }
-      if (board[i][j] >= 0) {
-        for (let k = -1; k <= 1; k++) {
-          for (let l = -1; l <= 1; l++) {
-            if ((i + k) >= 0 && (j + l) >= 0 && (i + k) < width && (j + l) < height) {
-              if (board[i + k][j + l] == -1) {
-                board[i][j]++;
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-  return board;
-}
-
-function waterfall(x, y, board, revealed) {
-  if(board[x][y] > 0&&!revealed[x][y]){
-    revealed[x][y] = true;
-    return [{x:x, y:y, val:board[x][y]}];
+function waterfall(x, y, game) {
+  if(game.board[x][y] > 0&&!game.revealed[x][y]){
+    game.reveal(x,y);
+    return [{x:x, y:y, val:game.board[x][y]}];
   }
   let out = [];
   for (let i = -1; i <= 1; i++) {
     for (let j = -1; j <= 1; j++) {
       if (!(i == 0 && j == 0) && x + i >= 0 && y + j >= 0 && x + i < width && y + j < height) {
-        if (board[x + i][y + j] >= 0) {
-          if (board[x + i][y + j] == 0 && revealed[x + i][y + j] == false) {
-            revealed[x + i][y + j] = true;
-            out = concat(out, waterfall(x + i, y + j, board, revealed));
+        if (game.board[x + i][y + j] >= 0) {
+          if (game.board[x + i][y + j] == 0 && game.revealed[x + i][y + j] == false) {
+            game.reveal(x + i, y + j);
+            out = concat(out, waterfall(x + i, y + j, game));
           }
-          revealed[x + i][y + j] = true;
-          out.push({ x: x + i, y: y + j, val: board[x + i][y + j] });
+          game.reveal(x + i, y + j);
+          out.push({ x: x + i, y: y + j, val: game.board[x + i][y + j] });
         }
       }
     }
